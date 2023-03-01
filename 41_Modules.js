6105c3afe0a768a1a176b8c23a620e63ef72dc85
Sahil <kingcodderno1@gmail.com>
2023-03-01 16:36:17 +0530

    3/1/2023

---
+++ 41_Modules.js
@@ -0,0 +1,24 @@
+const greet = (()=>{
+    console.log("Hellow World");
+})
+// const greetME = ((Name)=>{
+//     console.log("Hellow World "+Name);
+// })
+
+
+// let a = 100
+module.exports= greet;
+// module.exports= {greet,greetME};
+// module.exports= {greet,greetME,a};
+
+// ES6 Modules
+// export const greet = (()=>{
+//     console.log("Hellow World");
+// })
+// export const greetME = ((Name)=>{
+//     console.log("Hellow World "+Name);
+// })
+// const greetYou = (()=>{
+//     console.log("Hellow you");
+// })
+// export default greetYou;
---
+++ 42_ModuleUse.js
@@ -0,0 +1,16 @@
+const greet = require("./41_Modules");
+
+// greet.greet()
+// greet.greetME("Sahil")
+
+// const {greet,greetME,a} = require("./41_Modules");
+
+greet()
+// greetME("Sahil")
+// console.log(a);
+
+// ES6 Modules use
+// import greetYou,{ greet, greetME } from "./41_Modules.js";
+// greet()
+// greetME("Sahil")
+// greetYou()
